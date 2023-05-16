4ae62e94ce87b8e2a9d5dd50083d3d3a90f6ead0
JustChung <86175184+JustChung@users.noreply.github.com>
2023-05-16 15:25:28 -0700

    improved formatting of main

--- CS2520-Assignment7/cannon/main.py
+++ CS2520-Assignment7/cannon/main.py
@@ -1,30 +1,23 @@
 from manager import *
 
-pg.init()
-pg.font.init()
+if __name__ == '__main__':
+    pg.init()
+    pg.font.init()
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
+    screen = pg.display.set_mode(SCREEN_SIZE)
+    pg.display.set_caption("Cannon Fodder")
 
-SCREEN_SIZE = (800, 600)
+    done = False
+    clock = pg.time.Clock()
 
+    mgr = Manager(n_targets=3)
 
-screen = pg.display.set_mode(SCREEN_SIZE)
-pg.display.set_caption("Cannon Fodder")
+    while not done:
+        clock.tick(15)
+        screen.fill(BLACK)
 
-done = False
-clock = pg.time.Clock()
+        done = mgr.process(pg.event.get(), screen)
 
-mgr = Manager(n_targets=3)
+        pg.display.flip()
 
-while not done:
-    clock.tick(15)
-    screen.fill(BLACK)
-
-    done = mgr.process(pg.event.get(), screen)
-
-    pg.display.flip()
-
-pg.quit()
+    pg.quit()
